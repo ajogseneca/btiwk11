07d0b49e73118157bbcc80e44cafaa63b6b11d2e
ajogseneca <ageorge55@myseneca.ca>
2023-03-28 15:48:14 -0400

    Done

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
+import ClickCounter from '../components/clickCounter' ///
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -20,6 +22,7 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
+          <p><ClickCounter /></p>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"

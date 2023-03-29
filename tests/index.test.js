b2d31610c2e3500d7194b26a21dd8c0dd474b65e
ajogseneca <ageorge55@myseneca.ca>
2023-03-29 13:57:52 -0400

    Done

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,28 +2,31 @@ import { render } from '@testing-library/react';
 import Home from '@/pages';
 
 test("renders at least one link to https://github.com in the first child of 'main' ", () => {
-  const {container} = render(<Home/>);
+  const { getByRole } = render(<Home />);
 
-  // attempt to fetch the first child element within the "main" element
-  const child = container.querySelector('main *');
+  // Get the main element
+  const mainElement = getByRole("main");
 
-  // ensure the the child element exists
+  // Get the first child element of the main element
+  const child = mainElement.querySelector("*");
+
+  // Ensure the child element exists
   expect(child).toBeTruthy();
 
-  // get all "a" elements within the child element
+  // Get all links within the child element
   const childLinks = child.querySelectorAll("a");
 
-  // ensure there is at least 1 "a" element within the child
-  expect(childLinks.length).toBeGreaterThan(0); // at least one link
+  // Ensure there is at least one link
+  expect(childLinks.length).toBeGreaterThan(0);
 
-  // count the number of links that include "https://vercel.com"
-  let vercelLinks = 0;
-
+  // Count the number of links that include "https://github.com"
+  let githubLinks = 0;
   childLinks.forEach(link => {
-    if(link.href.includes("https://github.com"))
-      vercelLinks++;
+    if (link.href.includes("https://github.com")) {
+      githubLinks++;
+    }
   });
 
-  // ensure that there is at least one link that includes "https://vercel.com"
-  expect(vercelLinks).toBeGreaterThan(0); // at least one vercel link
+  // Ensure there is at least one link to "https://github.com"
+  expect(githubLinks).toBeGreaterThan(0);
 });
